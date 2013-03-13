c837c72d5b03c20060cbbdd3ef1d6d20d6ad1432
qm2013 <qm.zhang.2010@sis.smu.edu.sg>
2013-03-13 11:37:27 +0800

    Update initMenus.js

--- javascripts/initMenus.js
+++ javascripts/initMenus.js
@@ -1,16 +1,35 @@
-function prepareList() {
-    $('#expList').find('li:has(ul)')
-    .click( function(event) {
-        if (this == event.target) {
-            $(this).toggleClass('expanded');
-            $(this).children('ul').toggle('medium');
-        }
-        return false;
-    })
-    .addClass('collapsed')
-    .children('ul').hide();
-};
- 
-$(document).ready( function() {
-    prepareList()
-});
+function initMenus() {
+    if (!document.getElementsByTagName) return;
+	var aMenus = document.getElementsByTagName("LI");
+	for (var i = 0; i < aMenus.length; i++) {
+		var mclass = aMenus[i].className;
+		if (mclass.indexOf("treenode") > -1) {
+			var submenu = aMenus[i].childNodes;
+			for (var j = 0; j < submenu.length; j++) {
+				if (submenu[j].tagName == "A") {
+					submenu[j].onclick = function() {
+						var node = this.nextSibling;
+						while (1) {
+							if (node != null) {
+								if (node.tagName == "UL") {
+									var d = (node.style.display == "none")
+									node.style.display = (d) ? "block" : "none";
+									this.className = (d) ? "treeopen" : "treeclosed";
+									return false;
+								}
+								node = node.nextSibling;
+							} else {
+								return false;
+							}
+						}
+						return false;
+					}
+					submenu[j].className = (mclass.indexOf("open") > -1) ? "treeopen" : "treeclosed";
+				}
+				if (submenu[j].tagName == "UL")
+					submenu[j].style.display = (mclass.indexOf("open") > -1) ? "block" : "none";
+			}
+		}
+	}
+}
+window.onload = initMenus;
